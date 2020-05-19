f49aab2365393c71f18af1819816722d3b4185c8
benzuowudi <1103721772@qq.com>
2020-05-19 18:00:11 +0800

    update StatePatternDoor/

---
+++ StatePatternDoor/Assets/Script/State/DoorState.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorState
+{
+    public virtual void EnterState()
+    {
+
+    }
+
+    public virtual IEnumerator StayState()
+    {
+        yield return null;
+    }
+
+    public virtual IEnumerator LeaveState()
+    {
+        yield return null;
+    }
+}
